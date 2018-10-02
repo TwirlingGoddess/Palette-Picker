9690a69a60dc7c527b763ca1149e34723e7daa15
Lee Graham <graham.la3@gmail.com>
2018-10-01 21:06:54 -0600

    Finish Stylings

--- server.js
+++ server.js
@@ -1,12 +1,41 @@
 const express = require('express');
+// const body = require('./public/index.html');
 const app = express();
+const bodyParser = require('body-parser');
+const initial = ['#', '', 'f', '1', 'a', '1',  'e'].reduce((item, accu) => {
+  if(!item.length) {
+    item = Math.floor(Math.random() * 10)
+    return accu + item
+  }
+  else {
+    return item + accu
+  }
+}, '')
+
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Chat Box';
+app.locals.color = initial
+
 
 app.get('/', (request, response) => {
-  response.send('Oh hey Chat Box');
 });
+
+app.get('/api/v1/title', (request, response) => {
+  response.send(app.locals.title)
+})
+
+app.get('/api/v1/color', (request, response) => {
+  response.send(app.locals.color)
+})
+
+// app.put('/', (request, response) => {
+//   response.send('hy u durin');
+//   response.json(app.locals.title)
+// })
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
