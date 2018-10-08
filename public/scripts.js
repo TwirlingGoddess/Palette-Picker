755178958f025e059fc5a94bb707d30cd989befe
Lee Graham <graham.la3@gmail.com>
2018-10-07 22:43:13 -0600

    Dynamically added villages to the possible palette list of villages

--- public/scripts.js
+++ public/scripts.js
@@ -8,31 +8,29 @@ var button6 = document.querySelector('.button6');
 var button7 = document.querySelector('.button7');
 var rainbow = document.querySelector('.rainbows');
 var lockBox = document.querySelector('.lock-box');
+var addPaletteButton = document.querySelector('.add-palette-button');
+var addVillageButton = document.querySelector('.add-village-button');
+var palette = document.querySelector('.palette-name');
+var village = document.querySelector('.village-name');
+var villageList = document.querySelector('select');
 let lockedColorArray = []
 var unlockedColorArray = [button1, button2, button3, button4, button5, button6, button7]
 
 spinner.addEventListener('click', createRainbow);
 lockBox.addEventListener('click', lockThisHex)
+addPaletteButton.addEventListener('click', createPalette)
+addVillageButton.addEventListener('click', createVillage)
 
 function createRainbow() {
   unlockedColorArray.forEach(item => {
     if(item.value === 'unlocked'){
     let colorItem = document.querySelector(`.${item.name}`);
-
-      colorItem.style.background = generateColors()
+    let textItem = document.querySelector(`.text-${item.name}`);
+    let hexCode = generateColors()
+      colorItem.style.background = hexCode
+      textItem.innerText = hexCode
     }
-
-  // color1.style.background = generateColors()
-  // color2.style.background = generateColors()
-  // color3.style.background = generateColors()
-  // color4.style.background = generateColors()
-  // color5.style.background = generateColors()
-  // color6.style.background = generateColors()
-  // color7.style.background = generateColors()
-
   })
-  
-    
 }
 
 function generateColors() {
@@ -45,16 +43,36 @@ function lockThisHex(event) {
   let value = event.target.value;
   if(value === 'unlocked') {
     event.target.value = 'locked'
+    event.target.style.background = 'red'
     lockedColorArray.push(name);
-    console.log(name)
-    console.log(lockedColorArray, event.target.value)
   }
   if(value === 'locked') {
     const removeColorArray = lockedColorArray.filter(color => color !== name)
     event.target.value = 'unlocked'
+    event.target.style.background = 'lightgrey'
     lockedColorArray = [...removeColorArray]
-    console.log(lockedColorArray, event.target.value);
   } 
-  // const newStatus = (value ? 'unlocked' || 'locked')
-  // console.log(newStatus)
+}
+
+function createPalette(event) {
+  event.preventDefault();
+  if(palette.value) {
+      const hexCodeArray = unlockedColorArray.map(color => {
+        const hexNum = document.querySelector(`.text-${color.name}`)
+        return hexNum.innerText
+      })
+    const currentVillage = villageList.options[villageList.selectedIndex].text
+    console.log(currentVillage)
+    const newPaletteObject = { village: currentVillage, title: palette.value, colors: hexCodeArray}
+    console.log(newPaletteObject)
+    palette.value = ''}
+}
+
+function createVillage(event) {
+  event.preventDefault()
+  if(village.value) {
+    const newOption = document.createElement('option');
+    newOption.innerText = village.value
+    villageList.append(newOption);
+  }
 }
